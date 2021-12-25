11a0c182ab374396a638df2f5aa5a98b7215f0e4
Jimmy404 <bruno_pier@hotmail.com>
2021-12-25 20:55:53 -0300

    feat: adds playSingleRound function

--- src/app/app.js
+++ src/app/app.js
@@ -3,4 +3,30 @@ const computerPlay = () => {
   return choices[Math.floor(Math.random() * choices.length)];
 };
 
-console.log(computerPlay())
+const playSingleRound = (playerSelection) => {
+  let computerSelection = computerPlay();
+  let result = "";
+
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scisors" && computerSelection == "paper")
+  ) {
+    userScore++;
+    result = `You win! <br>You chose: ${playerSelection}<br>Your opponent chose: ${computerSelection}<br><br>Player score: ${userScore}<br>Computer score: ${computerScore}`;
+    if (userScore === 5) {
+      result += "<br>You won the best of 5! Congratulations!!!";
+      disableButton();
+    }
+  } else if (playerSelection === computerSelection) {
+    result = `Its a tie!<br>You chose: ${playerSelection}<br>Your opponent chose: ${computerSelection}<br><br>Player score: ${userScore}<br>Computer score: ${computerScore}`;
+  } else {
+    computerScore++;
+    result = `You lose!<br>You chose: ${playerSelection}<br>Your opponent chose: ${computerSelection}<br><br>Player score: ${userScore}<br>Computer score: ${computerScore}`;
+    if (computerScore === 5) {
+      result += `<br>Sorry! You lost the best of 5!`;
+      disableButton();
+    }
+  }
+  document.getElementById("result").innerHTML = result;
+};
